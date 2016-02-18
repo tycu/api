0144fab8055104a682aee6fa769974ae59fba249
Ryan Oldenburg <guzba8@gmail.com>
2016-02-17 22:05:06 -0800

    .

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.post('/v1/news', function(req, res) {
+app.get('/v1/news', function(req, res) {
     res.json({
-        
+
     });
 });
 
-var port = app.get('port') || 5000
+var port = process.env.PORT || 5000
 app.listen(port, function() {
     console.log('Example app listening on port ' + port);
 });
