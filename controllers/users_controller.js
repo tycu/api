2f3b725ce7ea76e53bee5a2f9658364e98d5dfec
tally-forth <matt@tally.us>
2016-08-10 01:47:27 -0400

    Clean up users controller.

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -10,56 +10,13 @@ var debug = require('debug')('controllers:users_controller:' + process.pid),
     utils = require("../services/tokenUtils.js"),
     SequelizeError = require(path.join(__dirname, "..", "errors", "SequelizeError.js"));
 
-
-
-  // app.get('/v1/users', function(req, res) {
-  //   var users = models.User.findAll();
-  //   users.then(function (value) {
-  //     res.json({users: value});
-  //   });
-  // });
-
-
-
-//   // used to be a POST to /v1/update-profile
-//   app.post('/v1/user/:userId', function(req, res) {
-
-//     models.User.findOne({where: { id: userId(req) }}).then(function(user, err) {
-
-
-//       console.log('user', user);
-//       console.log('req', req);
-
-
-//       if (req.body.name) {
-//         user.name = req.body.name
-//       }
-//       if (req.body.occupation) {
-//         user.occupation = req.body.occupation
-//       }
-//       if (req.body.employer) {
-//         user.employer = req.body.employer
-//       }
-//       if (req.body.streetAddress) {
-//         user.streetAddress = req.body.streetAddress
-//       }
-//       if (req.body.cityStateZip) {
-//         user.cityStateZip = req.body.cityStateZip
-//       }
-
-//       var now = Date.now() / 1000
-//       req.user.modified = now
-
-//       user.save()
-//       .then(function(anotherTask) {
-//         // you can now access the currently saved task with the variable anotherTask... nice!
-//       }).catch(function(error) {
-//         // Ooops, do some error-handling
-//     })
-
+// NOTE example of get all users call
+// app.get('/v1/users', function(req, res) {
+//   var users = models.User.findAll();
+//   users.then(function (value) {
+//     res.json({users: value});
 //   });
-// }
-
+// });
 
 function loadUser(req) {
   var userId = req.params['id'];
@@ -79,7 +36,6 @@ function loadUser(req) {
     where: { id: userId }
   })
 }
-
 
 function fetchUserInfo(req, res, next) {
   debug("fetchUserInfo");
@@ -145,6 +101,7 @@ module.exports = function() {
     return res.status(200).json(req.user);
   });
 
+  // used to be a POST to /v1/update-profile
   router.route("/users/:id").put(updateUserInfo, function(req, res, next) {
     debug('in PUT /users/:id');
     debug("userId: %s",req.params['id'])
