46ae6f1befc8c4bea41d24ce2cf814b29f57cd39
tally-forth <matt@tally.us>
2016-08-20 13:56:18 -0400

    First commit of cancan attempt setup.

--- models/index.js
+++ models/index.js
@@ -3,6 +3,9 @@
 const fs        = require('fs'),
       path      = require('path'),
       Sequelize = require('sequelize'),
+      cancan = require('cancan'),
+      can = cancan.can;
+
 var basename  = path.basename(module.filename),
     env       = process.env.NODE_ENV || 'development',
     config    = require('../config/database.json')[env],
@@ -30,6 +33,39 @@ Object.keys(db).forEach(function(modelName) {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
+
+  if (modelName === 'User') {
+    console.log("adding cancan permissions to User");
+
+    cancan.configure(db[modelName], function (user) {
+      // ALL actions: view, add, edit, destroy
+      // this.can('view', Event);
+
+      if (user.role === 'user') {
+
+        this.can('view', User, function(user) {
+          return this.id === user.id
+        });
+
+        // update own users info
+        // add/view own contribution info contribution
+      }
+
+
+      if (user.role === 'admin') {
+        // whitelist all: 'manage'
+        this.can('manage', Event);
+        this.can('manage', Politician);
+        this.can('view', EventTweet);
+        this.can('view', User);
+        this.can('manage', Pac);
+        this.can('manage', PacEvent);
+        this.can('manage', Politician);
+        this.can('destroy', Event);
+      }
+    });
+  }
+
 });
 
 db.sequelize = sequelize;
